721b65f0eed83f8e60e9fd5aefb39c2abc5aac52
Pavel Georgiev <ipavelgeorgiev@outlook.com>
2019-09-08 19:45:07 +0300

    Added First Steps In Coding Lab

---
+++ Programming Basics/1. First Steps In Coding/03. num1To20.js
@@ -0,0 +1,5 @@
+function numbersFrom1To20() {
+    console.log(`1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20`);
+}
+
+numbersFrom1To20()
---
+++ Programming Basics/1. First Steps In Coding/04. rectangleArea.js
@@ -0,0 +1,8 @@
+function rectangleArea(input){
+    let a = input.shift();
+    let b = input.shift();
+    let area = a * b;
+    console.log(area)
+}
+
+rectangleArea([2,7])
